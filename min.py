93b43c8ee78c5177e8bcaba1797da5cf107f11c7
danahladchenko <157510371+danahladchenko@users.noreply.github.com>
2024-04-03 17:31:15 +0000

    addd to main

--- min.py
+++ min.py
@@ -1,2 +1,6 @@
-hello = "hello, it's me"
-print(hello)
+#print(my name is my_name)
+my_name = 'Jeanne d`Arc'
+print('my name is', my_name)
+#print(my name is my_name)
+my_name = 'Dana'
+print('my name is', my_name)
