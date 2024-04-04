c990cdf60168090ebe61299863b51dc438fc6d79
danahladchenko <157510371+danahladchenko@users.noreply.github.com>
2024-04-04 13:52:13 +0000

    addd to main

--- hw#4.py
+++ hw#4.py
@@ -4,7 +4,7 @@ print(f"Great, that you are {user_mood}! What is your name?")
 user_name = input() 
 print(f"Hello, {user_name}! How old are you?") 
 user_age = int(input()) 
-birth_year_guess = 2024 - user_age
+birth_year_guess = 2023 - user_age
  
 print(f"I see, you were born in year {birth_year_guess}? Let me know if it is True or False?") 
 birth_year_check = input() 
@@ -12,4 +12,4 @@ birth_year_check = input()
 if birth_year_check.lower() == "true": 
     print(f"Great, then {birth_year_guess} is your birth year!") 
 else: 
-    print(" Double-check your information.") 
+    print(" Double-check your information.")
